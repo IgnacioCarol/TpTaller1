1a75ee99dfc6b62196192fe19ff77a968d23184f
licha <lisandrotorresetti@hotmail.com>
2020-11-01 17:36:17 -0300

    Hago que el enemigo no rebote contra los bordes de la escena principal

--- src/gameobjects/Enemy.cpp
+++ src/gameobjects/Enemy.cpp
@@ -19,20 +19,11 @@ void Enemy::walk() {
     if (xPosition >= camXPosition && xPosition <= camXPosition + 800){ //The enemy is on scene
         inScene = true;
     }
-    else if (inScene){ //The enemy was in scene but its over
-        inScene = false;
-    }
-    if (inScene) {
-        xPosition = ((xPosition += direction) < 0) ? 0 : xPosition;
 
-        if (xPosition == camXPosition){
-            direction = enemyVelocity;
-        }
-        else if (xPosition == camXPosition + 800){
-            direction = -enemyVelocity;
-        }
+    if (inScene) {
+        xPosition += direction;
         flipFlag = direction == enemyVelocity;
-    }  //TODO hacer que se de vuelta cuando chocque contra tuberia/plataforma
+    }
 }
 
 void Enemy::draw(SDL_Renderer *renderer, int cameraX, int cameraY, size_t width, size_t height) {
