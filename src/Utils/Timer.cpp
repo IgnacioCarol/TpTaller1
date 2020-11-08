b996cfef58bd10a801331ff19aec0ee27e9b525b
Ignacio Carol Lugones <icarol@fi.uba.ar>
2020-11-08 01:32:03 -0300

    Fix minor overflow bugs

--- src/Utils/Timer.cpp
+++ src/Utils/Timer.cpp
@@ -4,16 +4,16 @@
 
 #include "Timer.h"
 
-Timer::Timer(Uint32 stopSeconds) {
-    this->stopTime = stopSeconds;
+Timer::Timer(int32 stopTime) {
+    this->stopTime = stopTime;
 }
 
 void Timer::start() {
     this->startTime = SDL_GetTicks();
 }
 
-Uint32 Timer::getTimeSecond() {
-    if (startTime != 0) {
+int32 Timer::getTimeSecond() {
+    if (startTime > 0) {
         return this->stopTime - ((SDL_GetTicks() - startTime) / 1000);
     }
     return this->stopTime;
