d9e9b421381200c23c1425a6f0d3c9fe32f06366
d-bizari <daniel.bizari@mercadolibre.com>
2020-11-21 18:57:01 -0300

    refactor parser, added game_mode, ip_address and port

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "SDL2/SDL.h"
 #endif
 #include <iostream>
+#include <arpa/inet.h>
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 #include "logger/logger.h"
@@ -12,15 +13,54 @@
 #define FPS 40;
 const int DELAY_TIME = 1000.0f / FPS;
 Uint32 frameStart, frameTime;
-#define MAX_ARGS 2
 
-bool parseCLI(int argc, char * argv[], std::string * xmlPath) {
+typedef enum {SERVER, CLIENT} game_mode_t;
+#define MAX_ARGS 5 // app_name, xml_path, server/client, ip_address, port
+
+bool parseCLI(int argc, char * argv[], std::string * xmlPath, game_mode_t * mode, std::string * ipAddr, int * port) {
     if (argc > MAX_ARGS) {
         return false;
-    } else if (argc == 1) {
-        *xmlPath = "./resources/config.xml"; // set default xml
+    }
+
+    if (argc == 1) {
+        // set default values
+        *xmlPath = "./resources/config.xml";
+        *ipAddr = "127.0.0.1";
+        *port = 8080;
+        return true;
+    }
+    std::string tmp;
+
+    // Parse XML file
+    *xmlPath = argv[1];
+
+    // Parse server/client mode
+    tmp = argv[2];
+    if (tmp == "client") {
+        *mode = CLIENT;
+    } else if (tmp == "server") {
+        *mode = SERVER;
     } else {
-        *xmlPath = argv[1];
+        Logger::getInstance()->error("Invalid launch format, only server and client mode are available");
+        return false;
+    }
+
+    // Parse IP address
+    *ipAddr = argv[3];
+    struct sockaddr_in sa;
+    int result = inet_pton(AF_INET, ipAddr->c_str(), &(sa.sin_addr));
+    if(result < 0) {
+        Logger::getInstance()->error("Invalid IP address format");
+        return false;
+    }
+
+    // Parse Port
+    tmp = argv[4];
+    std::stringstream intPort(tmp);
+    intPort >> *port;
+    if(*port < 1) {
+        Logger::getInstance()->error("The PORT must be a postive int");
+        return false;
     }
 
     return true;
@@ -35,9 +75,12 @@ int main(int argc, char * argv[]) {
 //#endif
     Logger::getInstance()->info("System initializing...");
 
-    std::string xmlPath = "";
+    std::string xmlPath;
+    game_mode_t mode;
+    std::string ipAddr;
+    int         port;
 
-    if (!parseCLI(argc, argv, &xmlPath)) {
+    if (!parseCLI(argc, argv, &xmlPath, &mode, &ipAddr, &port)) {
         Logger::getInstance() -> error("Error: incorrect the program does not support the amount of CLI params");
         return EXIT_FAILURE;
     }
