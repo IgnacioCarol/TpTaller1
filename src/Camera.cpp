224b52225383e902a57ca7d88f34dbbfa9c9615d
d-bizari <daniel.bizari@mercadolibre.com>
2021-02-13 17:37:15 -0300

    fix scroll with observer mode

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -9,7 +9,7 @@ void Camera::update(std::vector<Player *> players) {
     int maxPos = 0;
     int minPos = 20000;
     for (Player* player: players){
-        if (player->getState() != "PAUSED"){
+        if (player->getState() != "PAUSED" && player->getState() != "DYING" && player->getState() != "DYING_FALLING"){
             int playerPos = player -> getXPosition();
             maxPos = (playerPos > maxPos) ? playerPos : maxPos;
             minPos = (playerPos < minPos) ? playerPos : minPos;
