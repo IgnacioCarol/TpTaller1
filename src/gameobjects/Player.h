a5238056cdcdc2b16e5f0f6e3f4bc95b62bde164
IgnacioCarol <icarol@fi.uba.ar>
2021-02-16 03:14:56 -0300

    Add missing override

--- src/gameobjects/Player.h
+++ src/gameobjects/Player.h
@@ -89,7 +89,7 @@ public:
 
     void die() override;
     void dieFalling() override;
-    void fall();
+    void fall() override;
     int getWidth() override;
 
     void setPlayerBig(bool playerBig);
